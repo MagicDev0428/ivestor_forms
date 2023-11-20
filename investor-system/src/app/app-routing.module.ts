ba3f82ca1289092248338b228cd823bc956ae8e3
MagicDev0428 <fullstackdever425@gmail.com>
2023-11-20 14:24:16 +0100

    creating profile component for showing user profile

--- investor-system/src/app/app-routing.module.ts
+++ investor-system/src/app/app-routing.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ListInvestorComponent } from './list-investor/list-investor.component';
 import { ManageInvestorComponent } from './manage-investor/manage-investor.component';
+import { ProfileComponent } from './components/profile/profile.component';
 
 const routes: Routes = [
   {path: 'list', component: ListInvestorComponent},
   {path: 'manage-investor', component: ManageInvestorComponent},
   {path: 'manage-investor/:id', component: ManageInvestorComponent},
-  {path: '', redirectTo: '/list', pathMatch: 'full'}
+  {path: '', redirectTo: '/list', pathMatch: 'full'},
+  {path: 'profile', component: ProfileComponent}
 ];
 
 @NgModule({
