e326e33a6f236c49371f201de1c59678eaa900f8
MagicDev0428 <fullstackdever425@gmail.com>
2023-11-23 01:01:48 +0530

    adding form validation with mock data of investor

--- investor-system/src/app/app.module.ts
+++ investor-system/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -35,6 +36,8 @@ import { CallbackComponent } from './components/callback/callback.component';
     AppRoutingModule,
     NgbModule,
     HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
     AuthModule.forRoot({
       domain: 'dev-i2zq17ecubzreyxk.us.auth0.com',
       clientId: 'aP2PzJ0D8mtsvscnuX02mEGPosfi832v',
--- investor-system/src/app/manage-investor/manage-investor.component.ts
+++ investor-system/src/app/manage-investor/manage-investor.component.ts
@@ -1,7 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, ParamMap } from '@angular/router';
-import { Investor } from '../interface/investor';
+import { Investor } from '../model/investor';
 import { Observable, map } from 'rxjs';
+import {
+  Validators,
+  FormGroup,
+  NonNullableFormBuilder,
+  FormControl,
+} from "@angular/forms";
+
+import { FormBuilder } from '@angular/forms';
+import { InvestorService } from '../service/investor.service';
 
 @Component({
   selector: 'app-manage-investor',
@@ -10,18 +19,68 @@ import { Observable, map } from 'rxjs';
 })
 export class ManageInvestorComponent implements OnInit {
 
-  selectedInvestor$!: Observable<Investor>;
-  constructor(private activatedRoute: ActivatedRoute) {
+  selectedInvestor$!: Observable<string | number>;
+  investor: Investor;
+
+  protected addInvestorForm: FormGroup;
+  protected submitted = false;
+  constructor(private activatedRoute: ActivatedRoute, private formBuilder: FormBuilder, private investorService: InvestorService) {
 
     this.selectedInvestor$ = activatedRoute.params.pipe(map(p => p['id']));
     this.selectedInvestor$.subscribe(res => {
       console.log(res);
+      this.investorService.getMockdata().subscribe({
+        next:(val) => {
+          console.log(val);
+          this.investor = val;
+          
+        }
+      })
     })
     
   }
+
   ngOnInit() {
   //   this.activatedRoute.paramMap.subscribe((params: ParamMap) =>  {
   //     this.selectedInvestor$ = params.get('id');
   // });
+  
+  this.addInvestorForm = this.formBuilder.group(
+    {
+      investorName: new FormControl("", Validators.required),
+      nickName: new FormControl(),
+      phone: new FormControl("", Validators.required),
+      email: new FormControl("", [Validators.required, Validators.email]),
+      address: new FormControl(),
+      zipCode: new FormControl(),
+      city: new FormControl(),
+      country: new FormControl(),
+      investorStatus: new FormControl(),
+      facebook: new FormControl(),
+      passport: new FormControl(),
+      beneficiaryName: new FormControl("", Validators.required),
+    });
+
+    
+    this.addInvestorForm.setValue({investorName: this.investor.investorName, nickName: '', phone: 12356, email: "",
+      address: "", zipCode: "", city: "", country: "", investorStatus: "", facebook: "", passport: "", beneficiaryName: ""
+,     });
+
+   }
+  
+
+   protected get InvestorFormControl() {
+    return this.addInvestorForm.controls;
+  }
+
+  protected onSubmit(): void {
+    this.submitted = true;
+
+    if (this.addInvestorForm.valid) {
+      console.log(
+        "Form Submitted succesfully!!!\n Check the values in browser console."
+      );
+      console.table(this.addInvestorForm.value);
+    }
   }
 }
